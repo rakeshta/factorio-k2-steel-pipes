3c422ee44060908fcba755bba9fa2d389729203a
Rakesh Ayyaswami <rakeshta@gmail.com>
2022-12-31 03:16:19 +1100

    steel pipe entity prototype & graphics

---
+++ data.lua
@@ -0,0 +1,11 @@
+--
+--  data.lua
+--  factorio-k2-steel-pipes
+--
+--  Created by Rakesh Ayyaswami on 31 Dec 2022.
+--
+
+--- Path to the mod's root directory
+k2_steel_pipes_path = "__k2-steel-pipes__/"
+
+require("prototypes.steel-pipe.index")
